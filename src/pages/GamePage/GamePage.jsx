a6a578525ceff1ec33689b7874d5673302036778
Olexandr-L45D <litvinenko1978aleks@gmail.com>
2025-07-29 14:33:13 +0300

    add theme

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -5,21 +5,33 @@ import GameTicTacToe from "../../components/GameTicTacToe/GameTicTacToe";
 
 function GamePage() {
   const [showGame, setShowGame] = useState(false);
+  const [settings, setSettings] = useState({ theme: "pooh" });
+  const theme = settings?.theme || "pooh"; // або "ariel"
+  const themeClass = `${css.themeContainer} ${css[theme]}`;
 
   return (
     <div className={css.container}>
-      <button className={css.btn} onClick={() => setShowGame(true)}>
-        Play tic-tac-toe
-      </button>
+      <div className={themeClass}>
+        <button className={css.btn} onClick={() => setShowGame(true)}>
+          Play tic-tac-toe
+        </button>
+        <select
+          onChange={e => setSettings({ theme: e.target.value })}
+          value={settings.theme}
+        >
+          <option value="pooh">Pooh</option>
+          <option value="ariel">Ariel</option>
+        </select>
 
-      {showGame && (
-        <GameTicTacToe
-          name="Olexandr"
-          age={8}
-          language="en"
-          settings={{ theme: "forest" }}
-        />
-      )}
+        {showGame && (
+          <GameTicTacToe
+            name="Olexandr"
+            age={8}
+            language="en"
+            settings={settings}
+          />
+        )}
+      </div>
     </div>
   );
 }
